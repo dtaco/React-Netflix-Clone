96e37bec5ffd8af08e94633257d977f0cfb051c9
David Tacoronte <d.tacoronte89@gmail.com>
2023-11-05 23:42:03 -0500

    working on video player component

--- src/pages/Player.jsx
+++ src/pages/Player.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import styled; from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+import { BsArrowLeft } from 'react-icons/bs';
+import video from '../assets/video.mp4';
+
 
 export default function Player(props) {
     
 
     return (
-        <>
-            
-        </>
+        <Container>
+            <div className="player">
+                <div className="back">
+                    <BsArrowLeft/>
+                </div>
+                <video src={video} autoPlay loop controls muted>
+                    
+                </video>
+            </div>
+        </Container>
     )
 }
+
+const Container = styled.div`
+
+`
