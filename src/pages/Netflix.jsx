2e0c0cf653d40c8c1b11d2c982c05fc5e5c66af7
David Tacoronte <d.tacoronte89@gmail.com>
2023-11-05 23:06:41 -0500

    finished hero page with correct styling. Now working on Player page.

--- src/pages/Netflix.jsx
+++ src/pages/Netflix.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import styled from 'styled-components';
 import Navbar from '../components/Navbar';
 import backgroundImage from "../assets/home.jpg";
 import MovieLogo from "../assets/homeTitle.webp";
@@ -15,7 +16,7 @@ export default function Netflix(props) {
     };
 
     return (
-        <div> 
+        <Container> 
             <Navbar isScrolled={isScrolled} />
             <div className="hero">
                 <img 
@@ -37,6 +38,56 @@ export default function Netflix(props) {
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        </Container>
+    );
 }
+
+const Container = styled.div`
+    background-color: black;
+    .hero {
+        position: relative;
+        .background-image {
+            filter: brightness(60%);
+        }
+        img {
+            height: 100vh;
+            width: 100vw; 
+        }
+        .container {
+            position: absolute;
+            bottom: 5rem;
+            .logo {
+                img {
+                    width: 100%;
+                    height: 100%;
+                    margin-left: 5rem;
+                }
+            }
+            .buttons {
+                margin: 5rem;
+                gap: 2rem;
+                button {
+                    font-size: 1.4rem;
+                    gap: 1rem;
+                    border-radius: 0%.2rem;
+                    padding: 0.5rem;
+                    padding-left: 2rem;
+                    padding-right: 2.4rem;
+                    border: none;
+                    cursor: pointer;
+                    transition: 0.3s ease-in-out;
+                    &:hover: {
+                        opacity: 0.8;
+                    }
+                    &:nth-of-type(2) {
+                        background-color: rgba(109,109,110,0.7);
+                        color: white;
+                        svg {
+                            font-size: 1.8rem;
+                        }
+                    }
+                }
+            } 
+        }
+    }
+`;
